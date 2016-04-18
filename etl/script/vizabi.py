32bf6541bd0a0d3e13f4a1c9d81aae6dbddd8775
Semio Zheng <prairy.long@gmail.com>
2016-04-18 11:15:52 +0800

    add domain quantiles

--- etl/script/vizabi.py
+++ etl/script/vizabi.py
@@ -173,10 +173,27 @@ def generate_metadata(c_all, concepts, meta2, area, outdir, oneset=False):
             print('no datapoints for ', i)
             continue
 
+        # domain and availability
         dm = [float(df[i].min()), float(df[i].max())]
         av = [int(df['time'].min()), int(df['time'].max())]
 
-        indb['indicatorsDB'][i].update({'domain': dm, 'availability': av})
+        # domain_quantiles_10_90:
+        # 1) sort by indicator value
+        # 2) remove top and bottom 10% of values (os if 100 points, remove 10 from top and bottom)
+        # 3) take first and last value of what's left as min and max in the property above.
+        values_sorted = df[i].sort_values().values
+        q_10 = np.round(len(values_sorted) / 10)
+        q_90 = -1 * q_10 - 1
+
+        # values_sorted = values_sorted[q_10:q_90]
+        # domain_quantiles_10_90 = [values_sorted.min(), values_sorted.max()]
+
+        domain_quantiles_10_90 = [values_sorted[q_10], values_sorted[q_90]]
+
+        indb['indicatorsDB'][i].update({
+            'domain': dm, 'availability': av,
+            'domain_quantiles_10_90': domain_quantiles_10_90
+        })
 
     # newdb = OrderedDict([[key, indb['indicatorsDB'][key]] for key in sorted(indb['indicatorsDB'].keys())])
     # indb['indicatorsDB'] = newdb
